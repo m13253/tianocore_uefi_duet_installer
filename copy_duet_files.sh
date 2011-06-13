fe205e7d47cd9e0849121ab354f9367e7fc54821
Keshav Amburay <the.ridikulus.rat@gmail.com>
2011-06-13 14:04:11 +0530

    Remove sudo usage and check for root, do not export env vars

--- copy_duet_files.sh
+++ copy_duet_files.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-export WD="${PWD}/"
-export EFI_DUET_DIR="${WD}/"
+WD="${PWD}/"
+EFI_DUET_DIR="${WD}/"
 
 PROCESS_CONTINUE="TRUE"
 
@@ -24,31 +24,38 @@ then
 	echo "--------"
 	echo "The possible arguments for DUET_BUILD are UDK_X64 and EDK_UEFI64 (in CAPS)."
 	echo "--------"
-	echo "You need to have the permission to run sudo command in order to use this script."
+	echo "You must run this script as root."
 	echo "--------"
 	echo
-	export PROCESS_CONTINUE=FALSE
+	PROCESS_CONTINUE=FALSE
 fi
+
 
 if [ "${PROCESS_CONTINUE}" = "TRUE" ]
 then
+	### check for root
+	if ! [ ${UID} -eq 0 ]; then 
+		echo "ERROR: Please run as root user!"
+		exit 1
+	fi
+	
 	if [ "${2}" = "" ]
 	then
 		echo "DUET_BUILD not defined"
 	fi
 fi
 
-export EFI_DUET_MP=${1}
-export DUET_BUILD=${2}
+EFI_DUET_MP="${1}"
+DUET_BUILD="${2}"
 
-export PROCESSOR="X64"
+PROCESSOR="X64"
 
-[ "${DUET_BUILD}" = "UDK_X64" ] && export SHELL_DIR="${EFI_DUET_DIR}/Shell/UDK_X64/"
+[ "${DUET_BUILD}" = "UDK_X64" ] && SHELL_DIR="${EFI_DUET_DIR}/Shell/UDK_X64/"
 
-[ "${DUET_BUILD}" = "EDK_UEFI64" ] && export SHELL_DIR="${EFI_DUET_DIR}/Shell/EDK_X64/"
+[ "${DUET_BUILD}" = "EDK_UEFI64" ] && SHELL_DIR="${EFI_DUET_DIR}/Shell/EDK_X64/"
 
-export EFILDR_DIR="${EFI_DUET_DIR}/Efildr/${DUET_BUILD}/"
-export EXTRAS_DIR="${EFI_DUET_DIR}/Extras/${PROCESSOR}/"
+EFILDR_DIR="${EFI_DUET_DIR}/Efildr/${DUET_BUILD}/"
+EXTRAS_DIR="${EFI_DUET_DIR}/Extras/${PROCESSOR}/"
 
 if [ "${PROCESS_CONTINUE}" = TRUE ]
 then
@@ -64,17 +71,17 @@ then
 	
 	set -x -e
 	
-	sudo cp --verbose "${EFILDR_DIR}/Efildr20" "${EFI_DUET_MP}/EFILDR20"
-	# sudo cp --verbose "${EFILDR_DIR}"/*.fv "${EFI_DUET_MP}/"
+	cp --verbose "${EFILDR_DIR}/Efildr20" "${EFI_DUET_MP}/EFILDR20"
+	# cp --verbose "${EFILDR_DIR}"/*.fv "${EFI_DUET_MP}/"
 	
-	sudo cp --verbose "${SHELL_DIR}/LoadFv.efi" "${EFI_DUET_MP}/LoadFv.efi" || true
-	sudo cp --verbose "${SHELL_DIR}/DumpBs.efi" "${EFI_DUET_MP}/DumpBs.efi" || true
+	cp --verbose "${SHELL_DIR}/LoadFv.efi" "${EFI_DUET_MP}/LoadFv.efi" || true
+	cp --verbose "${SHELL_DIR}/DumpBs.efi" "${EFI_DUET_MP}/DumpBs.efi" || true
 	
-	sudo mkdir -p "${EFI_DUET_MP}/efi/Shell"
-	sudo cp --verbose "${SHELL_DIR}/Shell_Full.efi" "${EFI_DUET_MP}/efi/Shell/Shell.efi"
+	mkdir -p "${EFI_DUET_MP}/efi/Shell"
+	cp --verbose "${SHELL_DIR}/Shell_Full.efi" "${EFI_DUET_MP}/efi/Shell/Shell.efi"
 	
-	sudo mkdir -p "${EFI_DUET_MP}/efi/extras"
-	sudo cp --verbose "${EXTRAS_DIR}"/*.efi "${EFI_DUET_MP}/efi/extras/" || true
+	mkdir -p "${EFI_DUET_MP}/efi/extras"
+	cp --verbose "${EXTRAS_DIR}"/*.efi "${EFI_DUET_MP}/efi/extras/" || true
 	
 	set +x +e
 	
